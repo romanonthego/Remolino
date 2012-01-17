dd4f7ced74f248631b09a6962fa41d8aa7937485
Dubinin Roman <dubininroman273@gmail.com>
2012-01-17 14:40:33 +0700

    rails 3.2 empty project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Remolino::Application.config.secret_token = '0e25ef2879489796492ace300cb5340bdc251f4b595037a5cf64c930599a5a933ce6d3a7851d8b1e94362e75046e4a736d89e2e7c15d41d3e2f482c0cbd49007'
